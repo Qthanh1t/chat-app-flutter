ce9b5cd617f4836d3062cfd6f72de81befe67af6
Quoc Thanh <thanhboks128@gmail.com>
2025-08-10 00:20:42 +0700

    call api and ui post post

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,6 +1,10 @@
 import 'package:chat_app/constants/api_constants.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:mime/mime.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:hive/hive.dart';
 import '../models/post_model.dart';
 import '../utils/avatar_name.dart';
@@ -15,6 +19,9 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   final box = Hive.box("chat_app");
   List<Post> _posts = [];
+  final TextEditingController _contentController = TextEditingController();
+  final List<File> _imageFiles = [];
+  bool _isLoading = false;
   @override
   void initState() {
     super.initState();
@@ -47,6 +54,113 @@ class _PostPageState extends State<PostPage> {
     }
   }
 
+  Future<void> pickImages() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+    if (result != null) {
+      setState(() {
+        _imageFiles.addAll(result.paths.map((path) => File(path!)));
+      });
+    }
+  }
+
+  Future<void> submitPost() async {
+    if (_contentController.text.trim().isEmpty) return;
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      FormData formData = FormData.fromMap({
+        'content': _contentController.text.trim(),
+        'images': await Future.wait(_imageFiles.map((img) async {
+          final mimeType = lookupMimeType(img.path);
+
+          // Bỏ qua file không phải ảnh
+          if (mimeType == null || !mimeType.startsWith('image/')) {
+            //print("❌ Bỏ qua file không phải ảnh: ${img.path}");
+            return null;
+          }
+
+          final typeSplit = mimeType.split('/'); // ['image', 'jpeg']
+          return await MultipartFile.fromFile(
+            img.path,
+            filename: img.path.split('/').last,
+            contentType: MediaType(typeSplit[0], typeSplit[1]),
+          );
+        })),
+      });
+
+      Dio dio = Dio();
+      final token = box.get('token');
+      dio.options.headers["Authorization"] = "Bearer $token";
+      final response = await dio.post(
+        "$baseUrl/posts/posts",
+        data: formData,
+        options: Options(contentType: 'multipart/form-data'),
+      );
+
+      if (response.statusCode == 201) {
+        setState(() {
+          fetchPost();
+        });
+      }
+      _contentController.clear();
+      _imageFiles.clear();
+    } catch (err) {
+      if (!mounted) return;
+      //print(err);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Đã xảy ra lỗi!"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  Widget _buildSelectedImagesPreview() {
+    return Wrap(
+      spacing: 8,
+      runSpacing: 8,
+      children: _imageFiles.asMap().entries.map((img) {
+        int index = img.key;
+        File file = img.value;
+        return Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.file(
+                file,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: InkWell(
+                onTap: () => setState(() {
+                  _imageFiles.removeAt(index);
+                }),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.black54, shape: BoxShape.circle),
+                  child: const Icon(Icons.close, color: Colors.white, size: 18),
+                ),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,8 +173,52 @@ class _PostPageState extends State<PostPage> {
           physics: const AlwaysScrollableScrollPhysics(),
           child: Column(
             children: [
-              //TODO phan tao bai viet
-
+              //phan tao bai viet
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _contentController,
+                      decoration: InputDecoration(
+                        hintText: "Bạn đang nghĩ gì",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 8),
+                      ),
+                      maxLines: null,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    if (_imageFiles.isNotEmpty) _buildSelectedImagesPreview(),
+                    Row(
+                      children: [
+                        TextButton(
+                            onPressed: pickImages,
+                            child: const Icon(Icons.photo)),
+                        const Spacer(),
+                        ElevatedButton(
+                          onPressed: _isLoading ? null : submitPost,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 12),
+                          ),
+                          child: _isLoading
+                              ? const SizedBox(
+                                  height: 18,
+                                  width: 18,
+                                  child:
+                                      CircularProgressIndicator(strokeWidth: 2),
+                                )
+                              : const Text('Đăng bài'),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
               //Phan hien thi bai viet
               ListView.builder(
                   shrinkWrap: true,
@@ -69,9 +227,10 @@ class _PostPageState extends State<PostPage> {
                   itemBuilder: (context, index) {
                     final post = _posts[index];
                     return Card(
-                      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 8),
                       child: Padding(
-                        padding: EdgeInsets.all(12),
+                        padding: const EdgeInsets.all(12),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
